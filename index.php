dc7681fece5316de41a75e605e8deffb80ba0733
martijn13795 <martijn13795@hotmail.com>
2016-05-12 20:55:28 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+
+<!-- Martijn || Datum: 2015 -->
+
+<html>
+	<head>
+		<title>Ubuntu server</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+			<div id="container">
+			<center>
+				<div id="header">
+					<div id="headtitle">
+						Martijn's Ubuntu server
+					</div>
+						<div id="headlinks">
+							<a href="http://martijnet.com">Martijnet</a>
+						
+							<a href="http://martijn.party:10000">WebMin</a>
+						
+							<a href="http://martijn.party/phpmyadmin">Phpmyadmin</a>
+						
+							<a href="http://martijn.party/phpInfo.php">PhpInfo</a>
+						</br></br>
+							<a href="http://martijn.party">Martijn.party</a>
+						
+							<a href="http://cmstorage.tk">CM-storage</a>
+						
+							<a href="https://martijn.party:8888">BitTorrent Sync</a>
+						
+							<a href="http://martijn.party/remoteCP_4-0-3-5">Trackmania Server</a>
+						</div>
+				</div>
+			</center>
+			</div>
+	</body>
+</html>
